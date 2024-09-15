142100bb2f74369eb00089c6cd8ce841f373edfa
gareth-cross <gcross.code@icloud.com>
2024-09-15 15:58:57 -0600

    Add webpack site

---
+++ site/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    watch: true,
+    watchOptions: {
+        ignored: '**/node_modules',
+    },
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
