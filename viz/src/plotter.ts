83054e35685f2a256ad14b0de123b7537ce75c78
gareth-cross <gcross.code@icloud.com>
2024-10-13 12:27:26 -0700

    Add a basic grid to the plotter

--- viz/src/plotter.ts
+++ viz/src/plotter.ts
@@ -16,10 +16,19 @@ class Range {
   }
 }
 
+export interface GridTickConfig {
+  major_interval: number;
+  num_minor_ticks: number;
+}
+
 // Parameters we can pass to `Plotter` to configure rendering.
 export interface PlotterConfig {
   yaxis_limit_lower?: number;
   yaxis_limit_upper?: number;
+
+  // Spacing between grid-ticks on the x and y axes.
+  grid_x?: GridTickConfig;
+  grid_y?: GridTickConfig;
 }
 
 // A simple plotter.
@@ -80,6 +89,9 @@ export class Plotter {
     const x_dst_range = new Range(0, ctxWidth);
     const y_dst_range = new Range(0, ctxHeight);
 
+    // Draw the background grid.
+    this.drawGrid({ x: x_range, y: y_range }, { x: x_dst_range, y: y_dst_range });
+
     // Draw some data:
     const { x: x_data, y: y_data } = this.data;
     console.assert(
@@ -87,12 +99,11 @@ export class Plotter {
       `X & Y data have mismatched lengths: ${x_data.length} != ${y_data.length}`
     );
 
-    // console.log(y_data);
-
     this.context.lineCap = 'round';
     this.context.lineJoin = 'round';
     this.context.strokeStyle = '#C3391B';
     this.context.lineWidth = 2;
+    this.context.setLineDash([]);
 
     const x_data_scaled: Float64Array = x_data.map((x) => {
       return x_dst_range.convertTo(x_range.convertFrom(x));
@@ -107,5 +118,76 @@ export class Plotter {
       this.context.lineTo(x_data_scaled[i], y_data_scaled[i]);
     }
     this.context.stroke();
+  }
+
+  // Draw a grid on the plot area using the parameters specified in `x_grid` and `y_grid`.
+  public drawGrid(src_range: { x: Range; y: Range }, dest_range: { x: Range; y: Range }) {
+    // Define a lambda we can reuse for x and y:
+    type DrawCallable = (position: number, is_major: boolean) => void;
+
+    const compute_grid_lines = (config: GridTickConfig, src: Range, callable: DrawCallable) => {
+      const { major_interval: major, num_minor_ticks: num_minor_ticks } = config;
+
+      console.assert(
+        num_minor_ticks >= 0 && major > 0,
+        `Invalid grid params: ${this.config.grid_x}`
+      );
+
+      for (var major_val = src.min + major; major_val < src.max; major_val += major) {
+        const major_val_normalized = src.convertFrom(major_val);
+        callable(major_val_normalized, true);
+
+        if (num_minor_ticks > 0) {
+          // Compute how much space between each tick.
+          const spacing = major / (num_minor_ticks + 1);
+          for (var i = 0; i < num_minor_ticks; ++i) {
+            const minor_val_normalized = src.convertFrom((i + 1) * spacing + major_val);
+            callable(minor_val_normalized, false);
+          }
+        }
+      }
+    };
+
+    this.context.lineCap = 'round';
+    this.context.lineJoin = 'round';
+    this.context.strokeStyle = 'rgb(244 114 182)';
+
+    if (this.config.grid_x) {
+      const start_y = dest_range.y.convertTo(0);
+      const end_y = dest_range.y.convertTo(1.0);
+
+      compute_grid_lines(this.config.grid_x, src_range.x, (x_normalized, is_major) => {
+        if (is_major) {
+          this.context.lineWidth = 0.5;
+          this.context.setLineDash([]);
+        } else {
+          this.context.lineWidth = 0.25;
+          this.context.setLineDash([5, 5]);
+        }
+        this.context.beginPath();
+        this.context.moveTo(dest_range.x.convertTo(x_normalized), start_y);
+        this.context.lineTo(dest_range.x.convertTo(x_normalized), end_y);
+        this.context.stroke();
+      });
+    }
+
+    if (this.config.grid_y) {
+      const start_x = dest_range.x.convertTo(0);
+      const end_x = dest_range.x.convertTo(1.0);
+
+      compute_grid_lines(this.config.grid_y, src_range.y, (y_normalized, is_major) => {
+        if (is_major) {
+          this.context.lineWidth = 0.5;
+          this.context.setLineDash([]);
+        } else {
+          this.context.lineWidth = 0.25;
+          this.context.setLineDash([5, 5]);
+        }
+        this.context.beginPath();
+        this.context.moveTo(start_x, dest_range.y.convertTo(y_normalized));
+        this.context.lineTo(end_x, dest_range.y.convertTo(y_normalized));
+        this.context.stroke();
+      });
+    }
   }
 }
