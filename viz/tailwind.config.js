ed87864933f23aa806cc4d03e80fa4342339ed69
gareth-cross <gcross.code@icloud.com>
2024-10-24 01:27:54 -0700

    Minor cleanup to the gui

--- viz/tailwind.config.js
+++ viz/tailwind.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-    content: ['./src/*.html'],
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-    safelist: [
-        'text-slate-100', 'w-full', 'h-full', 'pr-2', 'pt-1'
-    ],
-}
+  content: ['./src/*.html'],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '700px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+  safelist: ['text-slate-100', 'w-full', 'h-full', 'pr-2', 'pt-1']
+};
